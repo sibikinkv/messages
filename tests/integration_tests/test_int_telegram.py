4c49ef61d98f59d5bdf461b94d7a3932abee997a
Stakhiev-Alexander <sashastakhiev@gmail.com>
2021-03-27 22:11:06 +0300

    Add integration test for Telegram

--- tests/integration_tests/test_int_telegram.py
+++ tests/integration_tests/test_int_telegram.py
@@ -33,6 +33,12 @@ def test_telegram_with_empty_subject(get_twilio):
     t.send()
 
 
+def test_telegram_with_attachment(get_twilio):
+    t = get_telegram
+    t.attachments = 'https://sun9-26.userapi.com/impf/QfKkHYUE8b1ZLb-7RPgrBqZlhNiBM5Sv9DLpAA/CoC8rWCVHlw.jpg?size=800x800&quality=96&sign=b26802ca7be47a62a27e009247e55a42&type=album'
+    t.send()
+
+
 def test_telegram_with_attachment_and_empty_body(get_twilio):
     t = get_telegram
     t.body = ''
