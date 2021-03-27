f1dda4326b43989f5ea101314610b2848b3c765e
JeRusakow <yegorsr0@gmail.com>
2021-03-27 21:10:20 +0300

    Errors fixed

---
+++ tests/integration_tests/int_setup.py
@@ -0,0 +1,9 @@
+import jsonconfig
+
+def integration_test_configured(msgtype):
+    with jsonconfig.Config('messages') as cfg:
+        data = cfg.data
+        return (
+            'integration_tester' in data.keys()
+            and msgtype in data['integration_tester']
+        )
